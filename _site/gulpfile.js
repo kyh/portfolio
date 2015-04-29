4b66cda8e8b427032b054e3736660f314c61e124
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-29 19:21:30 -0400

    Clean up HTML, removed comments

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -3,12 +3,20 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
-gulp.task('compress', function() {
-  return gulp.src('js/lib/*.js')
+gulp.task('compress-site', function() {
+  return gulp.src('js/lib/site/*.js')
   	.pipe(concat('app.js'))
   	.pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['compress']);
+gulp.task('compress-cube', function() {
+  return gulp.src('js/lib/cube/*.js')
+  	.pipe(concat('cube.js'))
+  	.pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('default', ['compress-site', 'compress-cube']);
