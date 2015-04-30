69f9741dcb77892e4b26f79b5bc6bc6c261fdb79
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-30 00:38:37 -0400

    Fixed routing issues, fixed cube rendering issue

--- js/lib/site/main.js
+++ js/lib/site/main.js
@@ -1,8 +1,8 @@
 ;(function($) {
   'use strict';
 
-  var baseURL       = 'portfolio-jekyll';
-  var siteLocation  = location.origin || location.protocol + "//" + location.host + baseURL;
+  var baseURL       = '/portfolio-jekyll';
+  var siteLocation  = (location.origin || location.protocol + "//" + location.host) + baseURL;
   var transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
   var menuOpen      = 0;
 
